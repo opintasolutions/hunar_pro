16970706d72bf6b466f971af026a41cfb27e8f29
CoderPrans <pranavbhaskar17@gmail.com>
2019-02-10 20:24:47 +0530

    Updates

---
+++ precache-manifest.3be2364c7e1add47dac9d6ca0d12e693.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b82f3968c45a95b6b11",
+    "url": "/hunar_pro/static/js/runtime~main.8b82f396.js"
+  },
+  {
+    "revision": "1261c416747a192dc607",
+    "url": "/hunar_pro/static/js/main.1261c416.chunk.js"
+  },
+  {
+    "revision": "a852b0ebc3c774f96352",
+    "url": "/hunar_pro/static/js/1.a852b0eb.chunk.js"
+  },
+  {
+    "revision": "1261c416747a192dc607",
+    "url": "/hunar_pro/static/css/main.2a33abe7.chunk.css"
+  },
+  {
+    "revision": "2f16b6b386cc1f920aaaa7b6fca2e75e",
+    "url": "/hunar_pro/index.html"
+  }
+];
